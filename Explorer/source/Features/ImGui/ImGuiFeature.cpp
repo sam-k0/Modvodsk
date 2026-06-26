8cbff0ba8da229c45488440d335043795cfac919
sam-k0 <56673835+sam-k0@users.noreply.github.com>
2026-06-26 15:54:19 +0100

    Add "Run Command" widget

--- Explorer/source/Features/ImGui/ImGuiFeature.cpp
+++ Explorer/source/Features/ImGui/ImGuiFeature.cpp
@@ -1,4 +1,5 @@
 #include "ImGuiFeature.h"
+#include <regex>
 #include <format>
 using namespace Aurie;
 using namespace YYTK;
@@ -60,6 +61,8 @@ void FeatureImGui::ImGuiRoutine(FWFrame& FrameContext)
            
         DrawGlobalVariableExplorer();
 
+        DrawRunCommand();
+
         ImGui::Render();
         g_Context->OMSetRenderTargets(1, &g_RTV, NULL);
         ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
@@ -112,3 +115,161 @@ void FeatureImGui::DrawGlobalVariableExplorer()
     ImGui::End();
 }
 
+void FeatureImGui::DrawRunCommand()
+{
+    ImGui::Begin("Run Command");
+
+    ImGui::InputText("cmd", g_RunCommandBuffer, sizeof(g_RunCommandBuffer));
+
+    if (ImGui::Button("Run"))
+    {
+        std::string cmd = g_RunCommandBuffer;
+
+        if (FeatureImGui::RunCommand(cmd))
+        {
+            // Avoid duplicate of last command
+            if (g_CommandHistory.empty() || std::find(g_CommandHistory.begin(), g_CommandHistory.end(), cmd) == g_CommandHistory.end())
+            {
+                g_CommandHistory.push_back(cmd);
+
+                if (g_CommandHistory.size() > MAXCMDHIST)
+                    g_CommandHistory.pop_front();
+            }
+        }
+    }
+
+    ImGui::Separator();
+    ImGui::Text("Recently used:");
+
+    for (int i = (int)g_CommandHistory.size() - 1; i >= 0; --i)
+    {
+        const std::string& cmd = g_CommandHistory[i];
+
+        if (ImGui::Button(cmd.c_str()))
+        {
+            strncpy_s(g_RunCommandBuffer, cmd.c_str(), sizeof(g_RunCommandBuffer));
+            g_RunCommandBuffer[sizeof(g_RunCommandBuffer) - 1] = '\0'; // safety
+        }
+    }
+
+    ImGui::End();
+}
+
+
+// Private
+
+std::vector<std::string> FeatureImGui::Tokenize(const std::string& ref)
+{
+    std::vector<std::string> vResults;
+    size_t _beginFuncCall = ref.find_first_of('(');
+
+    if (_beginFuncCall == std::string::npos)
+    {
+        return {};
+    }
+
+    size_t _endFuncCall = ref.find_first_of(')');
+
+    if (_endFuncCall == std::string::npos)
+    {
+        return {};
+    }
+
+    // Function name
+    vResults.push_back(ref.substr(0, _beginFuncCall));
+
+    std::stringstream ss(ref.substr(_beginFuncCall + 1, _endFuncCall - _beginFuncCall));
+    std::string sCurItem;
+
+    while (std::getline(ss, sCurItem, ','))
+    {
+        sCurItem.erase(std::remove_if(sCurItem.begin(), sCurItem.end(), ::isspace), sCurItem.end());
+        if (sCurItem.find_first_of(')') != std::string::npos)
+        {
+            auto closePos = sCurItem.find_first_of(')');
+
+            sCurItem = sCurItem.substr(0, closePos);
+        }
+
+        if (!sCurItem.empty())
+            vResults.push_back(sCurItem);
+    }
+
+    return vResults;
+}
+
+bool FeatureImGui::RunCommand(const std::string& cmd)
+{
+    if (cmd.empty())
+        return false;
+
+    std::string cmdcopy = cmd;
+
+    std::regex regexAssignment(R"(global\.([a-zA-Z_]+)\s*=\s*(.*))");
+    std::regex regexPeek(R"(global\.([a-zA-Z_]+))");
+
+    if (std::regex_match(cmdcopy, regexAssignment))
+    {
+        cmdcopy = std::regex_replace(cmdcopy, regexAssignment, "variable_global_set(\"$1\", $2)");
+    }
+    else if (std::regex_match(cmdcopy, regexPeek))
+    {
+        cmdcopy = std::regex_replace(cmdcopy, regexPeek, "variable_global_get(\"$1\")");
+    }
+
+    std::regex validCall(R"(^[a-zA-Z_]\w*\(.*\)$)");
+
+    if (!std::regex_match(cmdcopy, validCall))
+    {
+        FeatureImGui::Interface->PrintWarning("Run Command: Invalid syntax!");
+        return false;
+    }
+
+    std::vector<std::string> tokens = Tokenize(cmdcopy);
+    if (tokens.empty())
+        return false;
+
+    const std::string& funcName = tokens[0];
+
+    // --- Prepare args ---
+    std::vector<RValue> args(tokens.size() - 1);
+
+    for (size_t i = 1; i < tokens.size(); i++)
+    {
+        const std::string& token = tokens[i];
+
+        if (std::regex_match(token, std::regex(R"(^-?\d+(\.\d+)?$)")))
+        {
+            args[i - 1] = std::stod(token);
+        }
+        else if (std::regex_match(token, std::regex(R"REGEX("([^"]*)")REGEX")))
+        {
+            args[i - 1] = RValue(token.substr(1, token.size() - 2));
+        }
+        else if (token == "true")
+        {
+            args[i - 1] = true;
+        }
+        else if (token == "false")
+        {
+            args[i - 1] = false;
+        }
+        else
+        {
+            FeatureImGui::Interface->PrintWarning("Run Command: Unknown token: " + token);
+            return false;
+        }
+    }
+
+    RValue result;
+    auto status = FeatureImGui::Interface->CallBuiltinEx(result, funcName.c_str(), nullptr, nullptr, args);
+    if (!AurieSuccess(status))
+    {
+        FeatureImGui::Interface->PrintWarning("Run Command: Call failed: " + funcName);
+        return false;
+    }
+
+    FeatureImGui::Interface->PrintInfo("Run Command: Result: %s", result.ToString());
+
+    return true;
+}
--- Explorer/source/Features/ImGui/ImGuiFeature.h
+++ Explorer/source/Features/ImGui/ImGuiFeature.h
@@ -4,6 +4,7 @@
 #include "imgui/imgui_impl_win32.h"
 #include <Windows.h>
 #include <d3d11.h>
+#include <deque>
 #include <YYToolkit/YYTK_Shared.hpp>
 #include "../Variables/VariablesFeature.h"
 
@@ -15,6 +16,9 @@ extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam
 // Custom WndProc to forward messages to ImGui
 LRESULT CALLBACK MyWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+// Maximum number of recent commands
+#define MAXCMDHIST 5 
+
 
 namespace ImGuiFeature {
 	// Vars
@@ -23,14 +27,16 @@ namespace ImGuiFeature {
 	static HWND g_hWnd = nullptr;
 	static ID3D11RenderTargetView* g_RTV = nullptr;
 	static WNDPROC g_OriginalWndProc = nullptr;
-	// Settings
+	// Status vars
 	static bool g_ImGuiInited = false;
 	static std::vector<VarInfo> g_GlobalVariables;
+	static std::deque<std::string> g_CommandHistory;
 
+	// Input buffers
 	static char g_GlobalVariableNameFilter[256] = "";
 	static char g_GlobalVariableTypeNameFilter[256] = "";
 	static char g_GlobalVariableValueFilter[256] = "";
-
+	static char g_RunCommandBuffer[256] = "";
 
 	class FeatureImGui {
 
@@ -40,6 +46,13 @@ namespace ImGuiFeature {
 		static void ImGuiRoutine(FWFrame& FrameContext);
 
 		static void DrawGlobalVariableExplorer();
+
+		static void DrawRunCommand();
+
+	private:
+		static std::vector<std::string> Tokenize(const std::string& ref);
+
+		static bool RunCommand(const std::string& cmd);
 	};
 
 }
